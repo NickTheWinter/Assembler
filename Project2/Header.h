92efe2a0debd374346bbebf4241ffd9aab00e5cf
Зименковни <@101-01>
2022-10-13 14:27:26 +0300

    Comparing

--- Project2/Header.h
+++ Project2/Header.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <stdio.h>
+#include <stdlib.h>
 int Sum(int, int);
 int Subtract(int, int);
 int Devide(int, int);
 int Amp(int, int);
 int CompareTwo(int, int);
 int CompareThree(int, int, int);
+int Remnant(int a, int b);
+int* Swap(int*, int, int);
+void ShowArray(int*,int);
--- Project2/Source.c
+++ Project2/Source.c
@@ -1,7 +1,10 @@
 #include "Header.h"
+#define n 3
 int main()
 {
-	printf("%d\n", CompareTwo(2, 5));
+	int* massiv[n] = calloc()
+	massiv = Swap(massiv, 2, 3);
+	ShowArray(massiv,n);
 	system("pause");
 }
 int sum(int a, int b)
@@ -28,13 +31,14 @@ int Subtract(int a, int b)
 }
 int Devide(int a, int b)
 {
-	/*_asm
+	_asm
 	{
 		mov eax, a
 		mov ebx, b
+		mov edx, 0
 		div ebx
 		mov a, eax
-	};*/
+	};
 	return a;
 }
 int Amp(int a, int b)
@@ -47,14 +51,121 @@ int Amp(int a, int b)
 		mov a, eax
 	};
 }
+int Remnant(int a, int b)
+{
+	_asm
+	{
+		mov eax, a
+		mov ebx, b
+		mov edx, 0
+		div ebx
+		mov a, edx
+	}
+	return a;
+}
 int CompareTwo(int a, int b)
 {
 	_asm
 	{
-		mov eax,a
-		mov ebx,b
-		cmp eax,ebx
-		mov eax, 2
+		mov eax, a
+		mov ebx, b
+		cmp eax, ebx
+		je equal
+		jg greater
+		jmp less
+
+
+		equal :
+		mov a, 0
+			jmp exit
+
+			greater :
+		mov a, 1
+			jmp exit
+
+			less :
+		mov a, 2
+			jmp exit
+
+			exit :
 	}
 	return a;
+}
+int CompareThree(int a, int b, int c)
+{
+	_asm
+	{
+		mov eax, a
+		mov ebx, b
+		mov ecx, c
+		cmp eax, ebx
+		je equal
+		jg greater
+		jmp less
+
+
+		equal :
+		cmp eax, ecx
+			je equal2
+			jg greater2
+			jmp less2
+
+			greater :
+		cmp eax, ecx
+			je equal2
+			jg greater2
+			jmp less2
+
+			less :
+		cmp ebx, ecx
+			je equal2
+			jg greater3
+			jmp less2
+
+
+
+		equal2:
+		mov a, 0
+			jmp exit
+
+			greater2 :
+		mov a, 1
+			jmp exit
+
+			less2 :
+		mov a, 3
+			jmp exit
+
+			greater3 :
+		mov a, 2
+			jmp exit
+
+			exit :
+	}
+	return a;
+}
+int* Swap(int* massiv, int m, int mm)
+{
+	int a = massiv[m], b = massiv[mm];
+	_asm
+	{
+		mov eax, a
+		mov ebx, b
+		mov ecx, eax
+		mov ebx, eax
+		mov ebx, ecx
+		mov a, eax
+		mov b, ebx
+	}
+	massiv[m] = a;
+	massiv[mm] = b;
+	return massiv;
+}
+void ShowArray(int* massiv)
+{
+	for (size_t i = 0; i < n; i++)
+	{
+		printf("%d ", massiv[i]);
+	}
+	printf("\n");
 }
