f86caace38a90e0f1740fecf5ef97713a17133c4
Зименковни <@101-01>
2022-10-17 16:41:17 +0300

    asd

--- Project2/Header.h
+++ Project2/Header.h
@@ -1,12 +1,32 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <time.h>
+#include <malloc.h>
 #include <stdlib.h>
+#include <string.h> 
+
+
 int Sum(int, int);
 int Subtract(int, int);
 int Devide(int, int);
-int Amp(int, int);
+int Mult(int, int);
 int CompareTwo(int, int);
 int CompareThree(int, int, int);
-int Remnant(int a, int b);
+int Remainder(int, int);
 int* Swap(int*, int, int);
-void ShowArray(int*,int);
+void ShowArray(int*, int);
+
+int Sum_C(int, int);
+int Subtract_C(int, int);
+int Devide_C(int, int);
+int Mult_C(int, int);
+int CompareTwo_C(int, int);
+int CompareThree_C(int, int, int);
+int Remainder_C(int, int);
+int* Swap_C(int*, int, int);
+
+typedef struct CSV {
+	int a;
+	int c;
+} csv;
--- Project2/Source.c
+++ Project2/Source.c
@@ -1,174 +1,205 @@
-#include "Header.h"
-#define n 5
-int main()
+﻿#include "Header.h"
+#include "Header1.h"
+int write_to_csv(int count, csv* data)
 {
-	int* massiv = calloc(n, sizeof(int));
-	for (size_t i = 0; i < n; i++)
+	int i = 0;
+	FILE* f = fopen("X:/test.csv", "w");
+	if (f == NULL)
 	{
-		massiv[i] = i + 1;
+		return -1;
 	}
-	massiv = Swap(massiv, 0, 4);
-	//printf("%d\n", CompareThree(1, 2, 3));
-	ShowArray(massiv,n);
-	system("pause");
+	for (i = 0; i < count; i++)
+	{
+		fprintf(f, "%d,%d\n", data[i].a, data[i].c);
+	}
+	fclose(f);
+	return 0;
 }
 
-void ShowArray(int* massiv)
-{
-	for (size_t i = 0; i < n; i++)
+main() {
+	csv data[8];
+
+	clock_t start, end;
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
 	{
-		printf("%d ", massiv[i]);
+		Sum(2, 2);
 	}
+	end = clock();
+	data[0].a = end - start;
+	printf("Sum = %d\n", end - start);
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Subtract(2, 2);
+	}
+	end = clock();
+	data[1].a = end - start;
+	printf("Subtract = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Devide(2, 2);
+	}
+	end = clock();
+	data[2].a = end - start;
+	printf("Devide = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Mult(2, 2);
+	}
+	end = clock();
+	data[3].a = end - start;
+	printf("Mult = %d\n", end - start);
+	/*start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		CompareTwo(2, 2);
+	}
+	end = clock();
+	data[4].a = end - start;
+	printf("CompareTwo = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		CompareThree(2, 2, 2);
+	}
+	end = clock();
+	data[5].a = end - start;
+	printf("CompareThree = %d\n", end - start);*/
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Remainder(2, 2);
+	}
+	end = clock();
+	data[6].a = end - start;
+	printf("Remainder = %d\n", end - start);
+	int* a = malloc(sizeof(int) * 5);
+	for (size_t i = 0; i < 5; i++)
+	{
+		a[i] = i;
+	}
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Swap(a, 0, 3);
+	}
+	end = clock();
+	data[7].a = end - start;
+	printf("Swap = %d\n", end - start);
+
 	printf("\n");
-}
-int sum(int a, int b)
-{
-	_asm
+
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
 	{
-		mov eax, a
-		mov ebx, b
-		add eax, ebx
-		mov a, eax
-	};
-	return a;
-}
-int Subtract(int a, int b)
-{
-	_asm
-	{
-		mov eax, a
-		mov ebx, b
-		sub eax, ebx
-		mov a, eax
-	};
-	return a;
-}
-int Devide(int a, int b)
-{
-	_asm
-	{
-		mov eax, a
-		mov ebx, b
-		mov edx, 0
-		div ebx
-		mov a, eax
-	};
-	return a;
-}
-int Amp(int a, int b)
-{
-	_asm
-	{
-		mov eax, a
-		mov ebx, b
-		mul ebx
-		mov a, eax
-	};
-}
-int Remnant(int a, int b)
-{
-	_asm
-	{
-		mov eax, a
-		mov ebx, b
-		mov edx, 0
-		div ebx
-		mov a, edx
+		Sum_C(2, 2);
 	}
-	return a;
-}
-int CompareTwo(int a, int b)
-{
-	_asm
+	end = clock();
+	data[0].c = end - start;
+	printf("Sum_C = %d\n", end - start);
+	for (size_t i = 0; i < 10000000; i++)
 	{
-		mov eax, a
-		mov ebx, b
-		cmp eax, ebx
-		je equal
-		jg greater
-		jmp less
-
-
-		equal :
-		mov a, 0
-			jmp exit
-
-			greater :
-		mov a, 1
-			jmp exit
-
-			less :
-		mov a, 2
-			jmp exit
-
-			exit :
+		Subtract_C(2, 2);
 	}
-	return a;
-}
-int CompareThree(int a, int b, int c)
-{
-	_asm
+	end = clock();
+	data[1].c = end - start;
+	printf("Subtract_C = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
 	{
-		mov eax, a
-		mov ebx, b
-		mov ecx, c
-		cmp eax, ebx
-		je equal
-		jg greater
-		jmp less
-
-
-		equal :
-		cmp eax, ecx
-			je equal2
-			jg greater2
-			jmp less2
-
-			greater :
-		cmp eax, ecx
-			je equal2
-			jg greater2
-			jmp less2
-
-			less :
-		cmp ebx, ecx
-			je equal2
-			jg greater3
-			jmp less2
-
-
-
-		equal2:
-		mov a, 0
-			jmp exit
-
-			greater2 :
-		mov a, 1
-			jmp exit
-
-			less2 :
-		mov a, 3
-			jmp exit
-
-			greater3 :
-		mov a, 2
+		Devide_C(2, 2);
 	}
-	return a;
-}
-int* Swap(int* massiv, int m, int mm)
-{
-	int a = massiv[m], b = massiv[mm];
-	_asm
+	end = clock();
+	data[2].c = end - start;
+	printf("Devide_C = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
 	{
-		mov eax, a
-		mov ebx, b
-		mov ecx, ebx
-		mov ebx, eax
-		mov eax, ecx
-		mov a, eax
-		mov b, ebx
+		Mult_C(2, 2);
 	}
-	massiv[m] = a;
-	massiv[mm] = b;
-	return massiv;
+	end = clock();
+	data[3].c = end - start;
+	printf("Mult_C = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		CompareTwo_C(2, 2);
+	}
+	end = clock();
+	data[4].c = end - start;
+	printf("CompareTwo_C = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		CompareThree_C(2, 2, 2);
+	}
+	end = clock();
+	data[5].c = end - start;
+	printf("CompareThree_C = %d\n", end - start);
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Remainder_C(2, 2);
+	}
+	end = clock();
+	data[6].c = end - start;
+	printf("Remainder_C = %d\n", end - start);
+	int* b = malloc(sizeof(int) * 5);
+	for (size_t i = 0; i < 5; i++)
+	{
+		b[i] = i;
+	}
+	start = clock();
+	for (size_t i = 0; i < 10000000; i++)
+	{
+		Swap_C(b, 0, 3);
+	}
+	end = clock();
+	data[7].c = end - start;
+	printf("Swap_C = %d\n", end - start);
+	end = clock();
+
+	write_to_csv(8, data);
+}
+
+
+int Sum_C(int a, int b) {
+	return a + b;
+}
+int Subtract_C(int a, int b) {
+	return a - b;
+}
+int Devide_C(int a, int b) {
+	return a / b;
+}
+int Mult_C(int a, int b) {
+	return a * b;
+}
+int CompareTwo_C(int a, int b) {
+	if (a > b)
+		return 1;
+	if (a < b)
+		return 2;
+	if (a == b)
+		return 0;
+}
+int CompareThree_C(int a, int b, int c) {
+	if (a > b || a > c)
+		return 1;
+	if (b > a || b > c)
+		return 2;
+	if (c > b || c > a)
+		return 3;
+	if (a == b || a == c)
+		return 0;
+};
+int Remainder_C(int a, int b) {
+	return a % b;
+}
+int* Swap_C(int* a, int i, int _i) {
+	int n = a[i];
+	a[i] = _i;
+	a[_i] = n;
 }
